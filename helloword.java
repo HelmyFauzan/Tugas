7381096d6393162a83d0ab13d3a3ec01c48af8ef
Helmy Fauzan Wahyudiputra <helmy.fauzan2506@gmail.com>
2022-10-11 19:19:12 +0700

    Tugas

---
+++ helloword.java
@@ -0,0 +1,9 @@
+/** 
+*Program pertama saya Helmy Fauzan Wahyudiputra 
+*/ 
+public class helloword { 
+public static void main(String args []){ 
+//Menampilkan kata "Welcome to java progamming" dilayar 
+System.out.println("Welcome to java progamming!"); 
+} 
+} 
