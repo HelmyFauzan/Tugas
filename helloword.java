8cd6524aad4ba3ca5d0e8ce9d1181f1513628af4
HelmyFauzan <115216286+HelmyFauzan@users.noreply.github.com>
2022-10-12 09:02:41 +0700

    update 1

--- helloword.java
+++ helloword.java
@@ -4,6 +4,7 @@
 public class helloword { 
 public static void main(String args []){ 
 //Menampilkan kata "Welcome to java progamming" dilayar 
-System.out.println("Welcome to java progamming!"); 
+System.out.println("Welcome to java progamming!");
+  System.out.println("Welcome to java progamming branch 1!");
 } 
 } 
